456e0157d2689f7ea0ef0b69c12edf97a6e1e1a0
AndrewCheDj <learnphs@gmail.com>
2019-06-03 19:27:39 +0300

    new

--- GetTiingoTickers.py
+++ GetTiingoTickers.py
@@ -1,10 +1,28 @@
+# библиотеки для записи CSV
 from tiingo import TiingoClient
 import csv
 
+# библиотеки для скачивания и анзипа котировок
+import requests
+import zipfile
+
+# скачивание и анзип котировок
+f = open(r'E:\code\tiingoCSV\tiingoDATA.zip', "wb")  # открываем файл для записи, в режиме wb
+ufr = requests.get("https://apimedia.tiingo.com/docs/tiingo/daily/supported_tickers.zip")  # делаем запрос
+f.write(ufr.content)  # записываем содержимое в файл
+f.close()
+
+z = zipfile.ZipFile('E:\\code\\tiingoCSV\\tiingoDATA.zip', 'r')
+z.printdir()
+z.extract('supported_tickers.csv')
+
+
+# всратые переменные
 a=0
 b=[]
 
-with open ("E:\\code\\tiingoCSV\\supported_tickers.csv", "r") as objcsv:
+# переработка котировок
+with open ("E:\\code\\tiingoCSV\\code\\supported_tickers.csv", "r") as objcsv:
    reader = csv.reader(objcsv)
    for row in reader:
       ticker = str(row[0])         # названия акций
@@ -24,7 +42,8 @@ with open("E:\\code\\tiingoCSV\\rec.csv", 'w', newline='') as f:
        if a > 1:  # отсечение первой строки так как в ней нет полезной для нас информации и пустых по дате строк
            quotes = client.get_ticker_price(tick, fmt='json', startDate=str(row[5]), frequency='daily')
            if quotes != []:     # отсекает встречающийся пустой список
-               print(b[a - 1])  # выводит название акции
+#               print(b[a - 1])  # выводит название акции
+               print ([tick, quotes[0]['high']])         # выводит название акции и котировку по заданному значению
                writer = csv.writer(f, delimiter=';')
                writer.writerow([tick, quotes[0]['high']])
 
